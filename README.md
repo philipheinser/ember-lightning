10e488d589e0125b1aa011b2cbe48fb4c6cc8a32
Philip Heinser <philipheinser@me.com>
2015-01-14 15:30:01 +0100

    added one click heroku

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ember-lightning
 Ember lightning hosting for https://github.com/LevelbossMike/ember-deploy
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/philipheinser/ember-lightning)
---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var redis = require('redis'),
+    coRedis = require('co-redis'),
+    router = require('koa-router'),
+    koa = require('koa');
+
+var app = koa(),
+    client  = redis.createClient(
+      process.env.REDIS_PORT,
+      process.env.REDIS_HOST
+    ),
+    dbCo = coRedis(client);
+
+client.auth(process.env.REDIS_SECRET);
+
+app.use(router(app));
+
+app.get('/', function* (next) {
+  console.log(process.env.APP_NAME);
+  var current = yield dbCo.get(process.env.APP_NAME +':current');
+  var index = yield dbCo.get(current);
+
+  this.body = index || '';
+});
+
+app.listen(process.env.PORT || 3000);
