6a24594fe7b62144ca139cf06fd3a5d1d4f99b2f
Mike Menne <mike@launchcode.org>
2016-05-05 09:47:19 +0000

    adding an init script for standing up the docker container with the appropriate environment variables

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Okay. Let's get things setup."
+
+echo "Ember Lightning servers up a single index.html file that references assets stored in Amazon Cloudfront"
+
+echo "\n################\n"
+echo "Okay.  Let's start up our ember-lightning server in Docker"
+
+(sudo docker build --rm --tag ember-lightning . && sudo docker run -d --restart=always -p 3700:3700 --name ember-lightning --env PORT=3700 --env APP_NAME=company-admin --env REDIS_HOST=$REDIS_HOST ember-lightning:latest ) 
+
+exit 0;
