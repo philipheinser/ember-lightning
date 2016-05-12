0017d8ea134562e93046394246c15c0f00bb37c4
EC2 Default User <ec2-user@ip-10-0-2-214.ec2.internal>
2016-05-12 19:51:11 +0000

    debugging

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "\n################\n"
 echo "Okay.  Let's start up our ember-lightning server in Docker"
 
 echo "WHat is REDIS HOST"
-echo $REDIS_HOST
+echo $1
 
 (sudo docker build --rm --tag ember-lightning . && sudo docker run -d --restart=always -p 3700:3700 --name ember-lightning --env PORT=3700 --env APP_NAME=company-admin --env REDIS_HOST=$REDIS_HOST ember-lightning:latest ) 
 
